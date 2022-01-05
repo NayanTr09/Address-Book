cafd1f23b38a77ae01821cce7621c3bc3a068581
NayanTr09 <nayantripathi42@gmail.com>
2022-01-05 08:20:35 +0530

    Added : Editing Contact Details functionality

--- src/com/addressbook/AddressBook.java
+++ src/com/addressbook/AddressBook.java
@@ -1,17 +1,19 @@
 package com.addressbook;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class AddressBook {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         System.out.println("This is a Address Book ");
-            ArrayList<Contacts> contact = new ArrayList<Contacts>();
-            Scanner in = new Scanner(System.in);
-            System.out.println("Enter number of persons: ");
-            int numofpersons = in.nextInt();
-
-            for(int i=0;i<numofpersons;i++){
+        ArrayList<Contacts> contact = new ArrayList<Contacts>();
+        Scanner in = new Scanner(System.in);
+        int Flag = 0;
+        while(Flag != 1) {
+            System.out.println("Kindly Enter choice \n 1. Add details to contact list \n 2. Edit details \n 3. Display details \n 4. Quit");
+            int AddDetails = in.nextInt();
+            if (AddDetails == 1) {
                 System.out.println("Enter first name");
                 String Firstname = in.next();
                 System.out.println("Enter last name");
@@ -24,11 +26,29 @@ public class AddressBook {
                 String Zip = in.next();
                 System.out.println("Enter email");
                 String Email = in.next();
-                Contacts details = new Contacts(Firstname,Lastname,Address,City,Zip,Email);
+                Contacts details = new Contacts(Firstname, Lastname, Address, City, Zip, Email);
                 contact.add(details);
+            } else if (AddDetails == 2) {
+                System.out.println("Enter first name of the user in contact list");
+                String Namelookup = in.next();
+                for (int i = 0; i < contact.size(); i++) {
+                    if (Namelookup.equals(String.valueOf(contact.get(i).getFirstname()))) {
+                        System.out.println("Enter updated first name");
+                        String UpdatedFirstName = in.next();
+                        contact.get(i).setFirstname(UpdatedFirstName);
+                        System.out.println(contact.get(i));
+                    }
+                }
             }
-            for(Contacts details : contact){
-                System.out.println(details.toString());
+            else if(AddDetails == 3) {
+                for (Contacts details : contact) {
+                    System.out.println(details.toString());
+                }
             }
+            else if (AddDetails == 4)
+            {
+                Flag=1;
+            }
+        }
     }
 }
--- src/com/addressbook/Contacts.java
+++ src/com/addressbook/Contacts.java
@@ -4,21 +4,36 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Contacts {
-    public String Firstname,Address,Email;
-    public String Lastname,City,Zip;
-    public Contacts(String Firstname2,String Lastname2,String Address2,String City2,String Zip2,String Email2){
+    public String Firstname, Address, Email;
+    public String Lastname, City, Zip;
+
+    public Contacts(String Firstname2, String Lastname2, String Address2, String City2, String Zip2, String Email2) {
         this.Firstname = Firstname2;
         this.Lastname = Lastname2;
         this.Address = Address2;
         this.Email = Email2;
         this.City = City2;
         this.Zip = Zip2;
-        System.out.println("Contact created with details :");
-        System.out.println("first name " +this.Firstname);
-        System.out.println("Second name " +this.Lastname);
-        System.out.println("Address " +this.Address);
-        System.out.println("City " +this.City);
-        System.out.println("Zip Code " +this.Zip);
-        System.out.println("Email " +this.Email);
+
+        System.out.println("New Contact added successfully with details :");
+        System.out.println("first name " + this.Firstname);
+        System.out.println("Second name " + this.Lastname);
+        System.out.println("Address " + this.Address);
+        System.out.println("City " + this.City);
+        System.out.println("Zip Code " + this.Zip);
+        System.out.println("Email " + this.Email);
+    }
+
+    public String getFirstname() {
+        return Firstname;
+    }
+
+    public void setFirstname(String FirstName) {
+        this.Firstname = FirstName;
+    }
+
+    public String toString() {
+
+        return "Contact [name=" + this.Firstname + ", address=" + this.Address + "]";
     }
 }
